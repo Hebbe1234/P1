d28f713a4a9fdf3ba5c066527bb571392f1d98b8
mart648g <martinpinholthansen@hotmail.com>
2019-11-20 10:02:04 +0100

    aded struct, vrker

--- main.c
+++ main.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 #include <math.h>
-
 #include "headers/main.h"
+#define NUMBER_OF_PASSENGERS_BOARDING 27
+#define NUMBER_OF_ROWS 10
+#define ENTRANCE_PLACEMENT 4 
 
 int main (void)
 {
 
+/*Defining passenger as a struct type */
+    typedef struct
+    {
+        int destination, 
+            location, 
+            finish, 
+            carry_on, 
+            wait_time, 
+            inteference_flag;
+        struct passenger* spotting;
+    } passenger;
+    
+
+/*Defining the aircraft as a struct type */
+    typedef struct 
+    {
+        passenger boarding[NUMBER_OF_PASSENGERS_BOARDING];
+        passenger v_left;
+        passenger h_right;
+        int entrance;
+        int inteference[2*NUMBER_OF_ROWS];
+    } aircraft ;
+    
+
+    passenger example;
+    passenger p1;
+
+    p1.destination = 54;
+    p1.location = 5;
+    p1.finish = 0;
+    p1.carry_on = 1;
+    p1.wait_time = 0;
+    p1.inteference_flag = 0;
+    p1.spotting = NULL;
+
+    example.destination = 55;
+    example.location = 4;
+    example.finish = 0;
+    example.carry_on = 1;
+    example.wait_time = 0;
+    example.inteference_flag = 0;
+    example.spotting = ((void*)&p1);
+
+
+    printf("%d ", example.spotting->destination);
+
+    
+    printf("%d %d %d %d %d", p1.inteference_flag,p1.finish,p1.destination,p1.location, p1.carry_on);
+
+
+
     return(0);
 }
+
+

7945876a1d55af7229b4c16468cbcb68fe854715
mart648g <martinpinholthansen@hotmail.com>
2019-11-21 10:03:32 +0100

    random array 1

--- queue_generator.c
+++ queue_generator.c
@@ -3,19 +3,66 @@
 #include <stdlib.h>
 #include "headers/main.h"
 #include <time.h>
-#define NUMBER_OF_PASSENGERS_BOARDING 27
+#define NUMBER_OF_PASSENGERS_BOARDING 60
 #define NUMBER_OF_ROWS 10
 #define ENTRANCE_PLACEMENT 4 
 
 void get_random_number(int *k,int *j);
 void get_random_array(int random_destination[]);
 
+/*Defining passenger as a struct type */
+typedef struct passenger
+{
+    int destination, 
+        location, 
+        finish, 
+        carry_on, 
+        wait_time, 
+        inteference_flag;
+    struct passenger* spotting;
+} passenger;
+
+/*Defining the aircraft as a struct type */
+typedef struct aircraft 
+{
+    passenger boarding[NUMBER_OF_PASSENGERS_BOARDING];
+    passenger v_left;
+    passenger h_right;
+    int entrance;
+    /*
+        TODO: This should not be statically sized. 
+        Use int* instead and dynamically allocate memory from a parameter given by the user
+    */
+    int inteference[2*NUMBER_OF_ROWS];
+} aircraft ;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(void){
+
+    passenger boarding[NUMBER_OF_PASSENGERS_BOARDING];
     int random_destination[60];
     srand(time(0));
 
     get_random_array(random_destination);
+
+
+
+
+
+
+
     return (0);
 }
 

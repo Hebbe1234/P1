493a7fa2651986f9de2d86045d4f8ccfa152732e
mart648g <martinpinholthansen@hotmail.com>
2019-11-22 10:02:30 +0100

    ...

--- source/queue_generator.c
+++ source/queue_generator.c
@@ -21,94 +21,135 @@
 
 /* TODO: These prototypes must be moved to a header file */
 void get_random_number(int *k,int *j);
-void get_random_array(int random_destination[]);
-void passenger_get_random_destination(int *random_destination, passenger boarding[]);
+void get_random_array(int random_destination[], int length_of_array);
+void passenger_get_random_destination(int *random_destination, passenger passengers [], int length_of_array);
 int find_illegal_seat(int i);
-void initialize_passenger_array(passenger boarding[]);
-void reset_passenger_array(passenger boarding[]);
+void initialize_passenger_array(passenger passengers [], int length_of_array);
+void reset_passenger_array(passenger passengers [], int length_of_array);
+void initialize_spotting(passenger passengers[], int length_of_array);
+void get_carryon(passenger passengers[], int length_of_array);
+
 
 int main(void){
-    int i, random_destination[60];
-    passenger boarding[NUMBER_OF_PASSENGERS_BOARDING];
+    int i;
+    int length_of_array = 57;
+
+    struct passenger *passengers;
+    passengers = (struct passenger*)calloc(NUMBER_OF_PASSENGERS_BOARDING, sizeof(passenger));
+
 
     srand(time(0));
+    
 
-    reset_passenger_array(boarding);
+    initialize_passenger_array(passengers, length_of_array);
 
-    initialize_passenger_array(boarding);
 
-    get_random_array(random_destination);
 
-    passenger_get_random_destination(random_destination, boarding);
+
 
     printf("\n\n\n");
     for(i=0; i<57; i=i+1) {
-        printf("%d , %d \n", boarding[i].destination, i);
+        printf("%d , %d \n", passengers[i].destination, i);
     }
     return (0);
 }
 
-
-/* Every passenger in boarding gets set to 0*/
-void reset_passenger_array(passenger boarding[]) {
-    int i;
-    /*TODO: Remove magic number 57*/
-    for (i = 0; i < 57; i=i+1) {
-        boarding[i].destination = 0;
-        boarding[i].location = 0;
-        boarding[i].carry_on = 0;
-        boarding[i].wait_time = 0;
-        boarding[i].finish = 0;
-        boarding[i].interference_flag = 0;
-        boarding[i].spotting = NULL;
-    }
-}
-
-
 /*Passengers look at each other, P0 looks a nothing, P1 looks at P0...*/
 /*Der skal gøres så alt sker herinde, dvs. at random destination sker, og carryon sker herinde. */
-void initialize_passenger_array(passenger boarding[]) {
-    int i = 0;
+void initialize_passenger_array(passenger passengers [], int length_of_array) {
+    int *random_destination; 
 
-    boarding[0].spotting = NULL;
-    /* TODO: Remove magic number 57 - function should take the length of the arrays as an input */
-    for(i=1; i<57; i=i+1)
-    {
-        boarding[i].spotting = &(boarding[i-1]);    
+    random_destination = (int*)calloc(length_of_array + (SEATS_PER_ROW/2), sizeof(int));
+
+    reset_passenger_array(passengers, length_of_array);
+    get_random_array(random_destination, length_of_array);
+    passenger_get_random_destination(random_destination, passengers, length_of_array);
+    initialize_spotting(passengers, length_of_array);
+    get_carryon(passengers, length_of_array);
+
+}
+
+void get_carryon(passenger passengers[], int length_of_array){
+    int i, j, carry_on_percentage;
+
+    carry_on_percentage = 70;
+
+    for(i = 0; i < length_of_array; i++){
+        j = ((rand() % 100)+1);
+        if(j <= carry_on_percentage) {
+            passengers[i].carry_on = 1;
+        }
+        else {
+            passengers[i].carry_on = 0;        
+        }
+    }
+
+
+/*  printf("\n\n\n");
+    for(i=0; i<57; i=i+1) {
+        printf("%d , %d \n", passengers[i].carry_on, i);
+    }
+*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Every passenger in passengers  gets set to 0*/
+void reset_passenger_array(passenger passengers [], int length_of_array) {
+    int i;
+    /*TODO: Remove magic number 57*/
+    for (i = 0; i < length_of_array; i=i+1) {
+        passengers [i].destination = 0;
+        passengers [i].location = -1;
+        passengers [i].carry_on = 0;
+        passengers [i].wait_time = 0;
+        passengers [i].finish = 0;
+        passengers [i].interference_flag = 0;
+        passengers [i].spotting = NULL;
     }
 }
 
 
-void passenger_get_random_destination(int *random_destination, passenger boarding[]) {
+void passenger_get_random_destination(int *random_destination, passenger passengers [], int length_of_array) {
     int *p_array, i;
 
     p_array = random_destination;
     /* TODO: Remove magic number and change i = i+1 to i++ */
-    for(i=0; i<57; i=i+1)
+    for(i=0; i < length_of_array; i=i+1)
     {
         while(*(p_array+i)== (find_illegal_seat(0)) || *(p_array+i) == (find_illegal_seat(1)) || *(p_array+i) == (find_illegal_seat(2))) {
             p_array = p_array+1;
         }
-        boarding[i].destination = *(p_array+i);
+        passengers [i].destination = *(p_array+i);
     }
 }
 
 
 
 int find_illegal_seat(int i) {
-    return ((ENTRANCE_PLACEMENT * SEATS_PER_ROW) + (SEATS_PER_ROW/2) + i);
+    int entrance_placement = 4;
+    return ((entrance_placement * SEATS_PER_ROW) + (SEATS_PER_ROW/2) + i);
 }
 
-
-/* TODO: Remove magic numbers */
-void get_random_array(int random_destination[]) {
+void get_random_array(int random_destination[], int length_of_array) {
     int i, j, k, tmp_plads;
-    i=j=k=tmp_plads=0;
+    i = j = k = tmp_plads=0;
 
-    for(i=0; i<60; i=i+1)
+    for(i = 0; (i < length_of_array + (SEATS_PER_ROW/2)); i=i+1)
         random_destination[i]=i;
 
-    for(i=0; i<1000000; i=i+1) {
+    for(i = 0; i < 1000000; i = i + 1) {
         get_random_number(&k,&j);
         tmp_plads = random_destination[k];
         random_destination[k] = random_destination[j];
@@ -116,13 +157,29 @@ void get_random_array(int random_destination[]) {
     }
 }
 
-/* TODO: This function needs a better name */
+
 void get_random_number(int *k,int *j) {
     *k = (rand() % 60);
     *j = (rand() % 60);
 }
 
 
+void initialize_spotting(passenger passengers[], int length_of_array) {
+    int i;
+
+    passengers [0].spotting = NULL;
+
+    for(i=1; i < length_of_array; i=i+1) {
+        passengers [i].spotting = &(passengers [i-1]);    
+    }
+
+ /*   
+    for(i=1; i<57; i=i+1) {
+        printf("%d , %d \n", passengers[i].spotting->destination, i);
+    }
+
+*/
+}
 
 
 

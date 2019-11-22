bed3158dea6beb5e49506d4deac596817973ee23
sherlockina <you@example.com>
2019-11-22 09:25:15 +0100

    finished tests (7)

--- tests/initializing_tests.c
+++ tests/initializing_tests.c
@@ -38,7 +38,7 @@ void testInitializing_first(CuTest *tc) {
 
 }
 
-/* If p1 is in queue and example is in location fo entrance */
+/* If p1 is in queue and example is in location of entrance */
 void testInitializing_second(CuTest *tc) { 
 
     passenger example;
@@ -137,6 +137,105 @@ void testInitializing_fourth(CuTest *tc) {
 
 }
 
+/* If p1 is in queue and example is in queue and both spotting NULL */
+void testInitializing_fifth(CuTest *tc) { 
+
+    passenger example;
+    passenger p1;
+    aircraft plane;
+    passenger test[2];
+
+    int E = 4;
+
+    p1.destination = 54;
+    p1.location = -1;
+    p1.finish = 0;
+    p1.spotting = NULL;
+
+    example.destination = 55;
+    example.location = -1;
+    example.finish = 0;
+    example.spotting = NULL;
+
+    plane.v_left = example;
+
+    test[0] = p1;
+    test[1] = example;
+    plane.entrance = E;
+
+    initialize_passenger(test, plane);
+
+    CuAssertTrue(tc, test[0].location == 4);
+    CuAssertTrue(tc, test[1].location == 4); 
+
+}
+
+/* If p1 is in queue and example is in queue and seeing eachother */
+void testInitializing_sixth(CuTest *tc) { 
+
+    passenger example;
+    passenger p1;
+    aircraft plane;
+    passenger test[2];
+
+    int E = 4;
+
+    p1.destination = 54;
+    p1.location = -1;
+    p1.finish = 0;
+    p1.spotting = &example;
+
+    example.destination = 55;
+    example.location = -1;
+    example.finish = 0;
+    example.spotting = &p1;
+
+    plane.v_left = example;
+
+    test[0] = p1;
+    test[1] = example;
+    plane.entrance = E;
+
+    initialize_passenger(test, plane);
+
+    CuAssertTrue(tc, test[0].location == -1);
+    CuAssertTrue(tc, test[1].location == -1); 
+
+}
+
+/* If p1 is in plane and example is in plane but finished */
+void testInitializing_seventh(CuTest *tc) { 
+
+    passenger example;
+    passenger p1;
+    aircraft plane;
+    passenger test[2];
+
+    int E = 4;
+
+    p1.destination = 54;
+    p1.location = -1;
+    p1.finish = 0;
+    p1.spotting = &example;
+
+    example.destination = 55;
+    example.location = E;
+    example.finish = 1;
+    example.spotting = NULL;
+
+    plane.v_left = example;
+
+    test[0] = p1;
+    test[1] = example;
+    plane.entrance = E;
+
+    initialize_passenger(test, plane);
+
+    CuAssertTrue(tc, test[0].location == -1);
+    CuAssertTrue(tc, test[1].location == 4); 
+
+}
+
 CuSuite *get_initialization_suit(void) /*Dette skal op i toppen af alltests.c*/
 {
     CuSuite *suite = CuSuiteNew();
@@ -144,5 +243,8 @@ CuSuite *get_initialization_suit(void) /*Dette skal op i toppen af alltests.c*/
     SUITE_ADD_TEST(suite, testInitializing_second);
     SUITE_ADD_TEST(suite, testInitializing_third);
     SUITE_ADD_TEST(suite, testInitializing_fourth);
+    SUITE_ADD_TEST(suite, testInitializing_fifth);
+    SUITE_ADD_TEST(suite, testInitializing_sixth);
+    SUITE_ADD_TEST(suite, testInitializing_seventh);
     return suite;
 }

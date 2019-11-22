7e92a4b84eaef7dd7866397ef87aca6126b30543
kristnaKris <kristian.d.s.thomsen@gmail.com>
2019-11-22 11:28:57 +0100

    Added print of intereference

--- source/print_aircraft.c
+++ source/print_aircraft.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Size_OF_INTERFERENCE 2*10
+#define SIZE_OF_INTERFERENCE 2*10
 
 void print_aircraft(aircraft plane) {
     int i;
@@ -18,7 +18,7 @@ void print_aircraft(aircraft plane) {
 
     printf(" Number of passengers: %d\n Location of last left person: %d\n location of last right person: %d\n Location of entrance: %d\n",
     O, V, H, E);
-    for (i = 0; i < Size_OF_INTERFERENCE; i++)
+    for (i = 0; i < SIZE_OF_INTERFERENCE; i++)
     {
         printf("%d\n", plane.interference[i]);
     }

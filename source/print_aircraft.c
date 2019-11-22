81125dd0c5a0f23fb0403a1a8edde9c14677c01b
sherlockina <you@example.com>
2019-11-22 10:01:18 +0100

    print aircraft function added

---
+++ source/print_aircraft.c
@@ -0,0 +1,12 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+#include "../headers/structs.h"
+
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_aircraft(aircraft plane) {
+    int O = sizeof(aircraft.boarding);
+}

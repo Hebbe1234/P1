c72e0c3b3d93589ebc076a8c200bc6a660910063
sherlockina <you@example.com>
2019-11-25 13:10:29 +0100

    fixes

--- source/movement.c
+++ source/movement.c
@@ -10,11 +10,12 @@
 /* function that defines the direction the passenger moves
     and changes passengers location */
 void movement (transition_system *t, int length) {
-    int i, dir = 0;
-    P_L = t->passengers[i].location;
-    P_D = t->passengers[i].destination;
-    P_S = t->passengers[i].spotting;
-    P_SL = t->passengers[i].spotting->location;
+    int i = 0, 
+        dir = 0;
+    int P_L = t->passengers[i].location;
+    int P_D = t->passengers[i].destination;
+    passenger *P_S = t->passengers[i].spotting;
+    int P_SL = t->passengers[i].spotting->location;
 
     for(i = 0; i < length; i++) {
         dir = (P_D - P_L) / abs(P_D - P_L); /* direction; positive is right, negative is left */
@@ -29,7 +30,7 @@ void movement (transition_system *t, int length) {
                 P_L += dir;
             }
             /* if going right and person spotting is more than one 'place' ahead */
-            else if (dir == 1 && P_SL > L + 1) {
+            else if (dir == 1 && P_SL > P_L + 1) {
                 P_L += dir;
             }
         }

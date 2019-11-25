2ecd2a4070308d3d91b71fbaf4ff28c4ffde90bd
jamstrupdk <Jamstr19@student.aau.dk>
2019-11-25 13:12:28 +0100

    before merge

--- source/movement.c
+++ source/movement.c
@@ -11,10 +11,10 @@
     and changes passengers location */
 void movement (transition_system *t, int length) {
     int i, dir = 0;
-    P_L = t->passengers[i].location;
-    P_D = t->passengers[i].destination;
-    P_S = t->passengers[i].spotting;
-    P_SL = t->passengers[i].spotting->location;
+    int P_L = t->passengers[i].location;
+    int P_D = t->passengers[i].destination;
+    int P_S = t->passengers[i].spotting;
+    int P_SL = t->passengers[i].spotting->location;
 
     for(i = 0; i < length; i++) {
         dir = (P_D - P_L) / abs(P_D - P_L); /* direction; positive is right, negative is left */

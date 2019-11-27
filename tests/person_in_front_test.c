124a905cf860258c65ad90653cf6ec1f46d66859
mart648g <martinpinholthansen@hotmail.com>
2019-11-27 08:29:55 +0100

    Færdig mange gode tests

--- tests/person_in_front_test.c
+++ tests/person_in_front_test.c
@@ -7,12 +7,12 @@
 
 
 void test_person_in_front(CuTest *tc) {
-    passenger p0, p1, p2, p3;
+    passenger p0, p1, p2, p3, p4;
     transition_system t;
 
     p0.destination = 54;
     p0.location = 9;
-    p0.finish = 0;
+    p0.finish = 1;
     p0.carry_on = 0;
     p0.wait_time = 0;
     p0.spotting = NULL;
@@ -33,23 +33,32 @@ void test_person_in_front(CuTest *tc) {
 
     p3.destination = 45;
     p3.location = 6;
-    p3.finish = 0;
+    p3.finish = 1;
     p3.carry_on = 0;
     p3.wait_time = 0;
     p3.spotting = &p2;
 
+    p4.destination = 45;
+    p4.location = 6;
+    p4.finish = 0;
+    p4.carry_on = 0;
+    p4.wait_time = 0;
+    p4.spotting = &p3;
+    
 
     t.passengers[0] = p0;
     t.passengers[1] = p1;
     t.passengers[2] = p2;
     t.passengers[3] = p3;
+    t.passengers[4] = p4;
 
-    person_in_front(&t, 4);
-
-    CuAssertTrue(tc, t.passengers[3].spotting->destination == 56);
-    CuAssertTrue(tc, t.passengers[2].spotting->destination == 55);
-
+    person_in_front(&t, 5);
     
+    CuAssertTrue(tc, t.passengers[4].spotting->destination == 55);
+    CuAssertTrue(tc, t.passengers[3].spotting->destination == 55);
+    CuAssertTrue(tc, t.passengers[2].spotting->destination == 55);
+    CuAssertTrue(tc, t.passengers[1].spotting == NULL); 
+
 }
 
 

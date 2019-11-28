407a375d2ab6e6954a93d641f46685ab7e817c47
Gustav Sneholm Bruhns <gustavsneholmbruhns@h145.aau1x.klient2.satellit.site.aau.dk>
2019-11-28 09:43:24 +0100

    En ekstra test

--- tests/rear_func_test.c
+++ tests/rear_func_test.c
@@ -103,7 +103,7 @@ void test_rear_manyperson(CuTest *tc)
     CuAssertTrue(tc, result == 1);
 
 }
-
+/* tests if both passengers went wrong way from entrance and have to pass eachother */
 void test_rear_weird(CuTest *tc)
 {
     passenger p1, p2;
@@ -130,6 +130,7 @@ void test_rear_weird(CuTest *tc)
     CuAssertTrue(tc, result == 0);
 }
 
+/* if passengers destination is entrance */
 void test_rear_forward(CuTest *tc)
 {
     passenger p1, p2, p3;
@@ -162,6 +163,7 @@ void test_rear_forward(CuTest *tc)
     CuAssertTrue(tc, result == -1);
 }
 
+/* if locations are wrong ( -1 or -4) */
 void test_rear_wronglocations(CuTest *tc)
 {
     int result;
@@ -205,6 +207,45 @@ void test_rear_wronglocations(CuTest *tc)
     CuAssertTrue(tc, result == 1);
 
 }
+/* If passengers to the left but initialized passenger must go to the right where there are no passengers */
+void test_rear_manyperson_but_noperson_to_right(CuTest *tc)
+{
+    int result;
+
+    passenger p0, p1, p2, p3;
+    transition_system t;
+
+    p0.destination = 10;
+    p0.location = 3;
+    p0.finish = 0;
+
+    p1.destination = 0;
+    p1.location = 2;
+    p1.finish = 0;
+
+    p2.destination = 2;
+    p2.location = 1;
+    p2.finish = 0;
+
+    p3.destination = 1;
+    p3.location = 0;
+    p3.finish = 0;
+
+    t.passengers[0] = p0;
+    t.passengers[1] = p1;
+    t.passengers[2] = p2;
+    t.passengers[3] = p3;
+ 
+
+    t.length = 5;
+    t.entrance = 3;
+    t.seats_row = 2;
+    t.rows= 7;
+    
+    result = rear_function(&t, 0);
+    CuAssertTrue(tc, result == -1);
+
+}
 
 CuSuite *get_rear_suit(void) /*Dette skal op i toppen af alltests.c*/
 {
@@ -215,5 +256,6 @@ CuSuite *get_rear_suit(void) /*Dette skal op i toppen af alltests.c*/
     SUITE_ADD_TEST(suite, test_rear_weird);
     SUITE_ADD_TEST(suite, test_rear_forward);
     SUITE_ADD_TEST(suite, test_rear_wronglocations);
+    SUITE_ADD_TEST(suite, test_rear_manyperson_but_noperson_to_right);
     return suite;
 }

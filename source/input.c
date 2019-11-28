39d7b9e585a9d2c5fa4bf1ff875cce1cb42ebeec
mart648g <56824309+mart648g@users.noreply.github.com>
2019-11-28 17:16:28 +0100

    Update source/input.c
    Co-Authored-By: Hebbe1234 <r.hebsgaard@gmail.com>

--- source/input.c
+++ source/input.c
@@ -27,7 +27,7 @@ transition_system input_t_system(void){
     printf("\nWrite movement time\n");
     scanf("%d", &struct_wait.t_m);
     
-    printf("\nWrite type 0 inteference\n");
+    printf("\nWrite type 0 interference time\n");
     scanf("%d", &struct_wait.t_0);
 
     printf("\nWrite type 1 interference time\n");

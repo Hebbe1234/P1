5e0960c610a0a17089e66624b59b89ad8d077f3a
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2019-11-29 09:02:50 +0100

    add function to print the values of a passenger in a single line

--- headers/print_passenger.h
+++ headers/print_passenger.h
@@ -6,3 +6,6 @@
 #endif
 /* This function takes a passenger as input and prints his current values */
 void print_passenger (passenger person);
+
+/* This function takes a passenger as input and prints his current values on a single line*/
+void print_passenger_line(passenger person);
--- source/print_passenger.c
+++ source/print_passenger.c
@@ -12,11 +12,26 @@ void print_passenger (passenger person) {
         P_L = person.location, 
         P_F = person.finish, 
         /* Finds the location of the person in front */
-        P_S = person.spotting->location,
+        P_S = person.spotting != NULL ? person.spotting->location : -100,
         P_C = person.carry_on, 
         P_W = person.wait_time, 
         P_I = person.interference_flag;        
 
-    printf(" Destination: %d\n Location: %d\n Finish: %d\n Location of passenger infront: %d\n Carry-on: %d\n Wait time: %d\n Interference: %d\n",
+    printf(" Destination: %d\n Location: %d\n Finished: %d\n Location of passenger infront: %d\n Carry-on: %d\n Wait time: %d\n Interference: %d\n",
+    P_D, P_L, P_F, P_S, P_C, P_W, P_I);
+}
+
+/* This function takes a passenger as input and prints his current values on a single line*/
+void print_passenger_line(passenger person) {
+    int P_D = person.destination, 
+        P_L = person.location, 
+        P_F = person.finish, 
+        /* Finds the location of the person in front */
+        P_S = person.spotting != NULL ? person.spotting->location : -100,
+        P_C = person.carry_on, 
+        P_W = person.wait_time, 
+        P_I = person.interference_flag;        
+
+    printf(" Destination: %d | Location: %d | Finished: %d | Location of passenger infront: %d | Carry-on: %d | Wait time: %d | Interference: %d\n",
     P_D, P_L, P_F, P_S, P_C, P_W, P_I);
 }

9cc4310220c9c58925ae8c2f9136982e7468b61e
mart648g <martinpinholthansen@hotmail.com>
2019-11-29 13:46:51 +0100

    færdig med ændring fra t til *t

--- tests/queue_generator_tests.c
+++ tests/queue_generator_tests.c
@@ -61,10 +61,9 @@ void test_is_legal_seat_E0(CuTest *tc) {
 
 /*Tests that a passenger either gets carry on 1, or 0*/
 void test_get_carryon_A57_CP50(CuTest *tc) {
-    struct passenger *passengers;
     transition_system t;
     int i, flag0;
-    passengers = (struct passenger*)calloc(57, sizeof(passenger));
+    t.passengers = (struct passenger*)calloc(57, sizeof(passenger));
     
     t.seats_per_row = 6; 
     t.length = 57;
@@ -73,19 +72,18 @@ void test_get_carryon_A57_CP50(CuTest *tc) {
     get_carryon(&t);
     
     for(i=0, flag0=1; (i<57) && (flag0==1); i++) {
-        if ((passengers[i].carry_on != 0) && (passengers[i].carry_on !=1))
+        if ((t.passengers[i].carry_on != 0) && (t.passengers[i].carry_on !=1))
             flag0=0;
     }
-    free(passengers);
+    free(t.passengers);
     CuAssertTrue(tc, flag0 == 1);
 }
 
 /*Tests that when carryon percentage is 100, everyone has carryon*/
 void test_get_carryon_A51_CP100(CuTest *tc) {
-    struct passenger *passengers;
     int i, flag1;
     transition_system t;
-    passengers = (struct passenger*)calloc(51, sizeof(passenger));
+    t.passengers = (struct passenger*)calloc(51, sizeof(passenger));
     
     t.seats_per_row = 6; 
     t.length = 51;
@@ -93,20 +91,19 @@ void test_get_carryon_A51_CP100(CuTest *tc) {
 
     get_carryon(&t);
     for(i=0, flag1=1; (i<51) && (flag1==1); i++) {
-        if (passengers[i].carry_on !=1)
+        if (t.passengers[i].carry_on !=1)
             flag1=0;
     }    
-    free(passengers);
+    free(t.passengers);
 
     CuAssertTrue(tc, flag1 == 1);
 }
 
 /*Tests that when carryon percentage is 0, noone has carryon*/
 void test_get_carryon_A51_CP0(CuTest *tc) {
-    struct passenger *passengers;
     int i, flag2;
     transition_system t;
-    passengers = (struct passenger*)calloc(51, sizeof(passenger));
+    t.passengers = (struct passenger*)calloc(51, sizeof(passenger));
     
     t.seats_per_row = 6; 
     t.length = 51;
@@ -114,10 +111,10 @@ void test_get_carryon_A51_CP0(CuTest *tc) {
     
     get_carryon(&t);
     for(i=0, flag2=1; (i<51) && (flag2==1); i++) {
-        if (passengers[i].carry_on !=0)
+        if (t.passengers[i].carry_on !=0)
             flag2=0;
     }    
-    free(passengers);
+    free(t.passengers);
 
     CuAssertTrue(tc, flag2 == 1);
 }
@@ -130,34 +127,35 @@ void test_get_carryon_A51_CP0(CuTest *tc) {
   Der testes at alle andre passasgere, kigger på den rigtige, 
   ved at tjekke at destinationen på den de kigger på, er korrekt*/
 void test_initialize_spotting(CuTest *tc) {
-    struct passenger *passengers;
     int i, flag1=1;
-
-    passengers = (struct passenger*)calloc(57, sizeof(passenger));
+    transition_system t;
+    t.passengers = (struct passenger*)calloc(57, sizeof(passenger));
+    t.length = 57;
 
     for (i = 0; i < 57; i++) {
-        passengers[i].destination = i;
+        t.passengers[i].destination = i;
     }
 
-    initialize_spotting(passengers, 57);
+    initialize_spotting(&t);
 
-    if(passengers[0].spotting!=NULL)
+    if(t.passengers[0].spotting!=NULL)
         flag1=0;
 
     for(i=1; i < 57; i=i+1) {
-        if(passengers[i].spotting->destination != passengers[i-1].destination)
+        if(t.passengers[i].spotting->destination != t.passengers[i-1].destination)
             flag1=0;
     }
-
+    free(t.passengers);
     CuAssertTrue(tc, flag1 == 1);
 }
 
 /* Tests that the 3 illegal seats dosent becomes a passengers destination*/
 /*And noone has the same destination*/
 void test_passenger_get_random_destinations_array57_entrance4(CuTest *tc) {
-    struct passenger *passengers;
     int i, j, flag0 = 1;
     int destination[60] = {0};
+    transition_system t;
+    t.length = 57;
 
     for(i=0; i<60; i++) {
         destination[i] = i;
@@ -166,21 +164,21 @@ void test_passenger_get_random_destinations_array57_entrance4(CuTest *tc) {
     destination[53] = -1;
     destination[0] = -1;
 
-    passengers = (struct passenger*)calloc(57, sizeof(passenger));
+    t.passengers = (struct passenger*)calloc(57, sizeof(passenger));
 
-    passenger_get_random_destinations(destination, passengers, 57, 4, 6);
+    passenger_get_random_destinations(destination, &t);
     for(i=0, flag0=1; i<57; i++){
-        if((passengers[i].destination == -1)) {
+        if((t.passengers[i].destination == -1)) {
             flag0=0;
         }
         for(j=0; j<57; j++){
             if(i==j)
                 j++;
-            if (passengers[i].destination == passengers[j].destination)
+            if (t.passengers[i].destination == t.passengers[j].destination)
                 flag0=0;
         }
     }
-    free(passengers);
+    free(t.passengers);
 
     CuAssertTrue(tc, flag0 == 1);
 }
@@ -189,65 +187,67 @@ void test_passenger_get_random_destinations_array57_entrance4(CuTest *tc) {
 /* Tests that the 3 illegal seats dosent becomes a passengers destination, by using 
 get_random_array, and noone has the same destination*/
 void test_passenger_get_random_destinations_array51_entrance0(CuTest *tc){
-    struct passenger *passengers;
     int i, j, flag1=1;
     int destination1[54] = {0};
-
-    passengers = (struct passenger*)calloc(51, sizeof(passenger));
+    transition_system t;
+    t.passengers = (struct passenger*)calloc(51, sizeof(passenger));
+    t.length = 51;
+    t.seats_per_row = 6;
+    t.entrance = 0; 
 
     for(i=0; i<54; i++) {
         destination1[i] = i;
     }
 
-    get_random_array(destination1,51, 6, 0);
+    get_random_array(destination1, &t);
 
-    passenger_get_random_destinations(destination1, passengers, 51, 0, 6);
+    passenger_get_random_destinations(destination1, &t);
 
     for(i=0, flag1=1; i<51; i++) {
-        if((passengers[i].destination == -1)) {
+        if((t.passengers[i].destination == -1)) {
             flag1=0;
         }
         for(j=0; j<51; j++) {
             if(i==j)
                 j++;
-            if (j<51 && (passengers[i].destination !=-1) && (passengers[j].destination !=-1) 
-                     && (passengers[i].destination == passengers[j].destination)){
+            if (j<51 && (t.passengers[i].destination !=-1) && (t.passengers[j].destination !=-1) 
+                     && (t.passengers[i].destination == t.passengers[j].destination)){
                 flag1=0;
             }
         }        
     }
     CuAssertTrue(tc, flag1 == 1);
+    free(t.passengers);
 }
 
 /*Tests that the reset array is correct*/
 void test_reset_passenger_array(CuTest *tc){
-
-    struct passenger *passengers;
     int i, flag0=1;
-
-    passengers = (struct passenger*)calloc(57, sizeof(passenger));
-
-    reset_passenger_array(passengers, 57);
+    transition_system t;
+    t.passengers = (struct passenger*)calloc(57, sizeof(passenger));
+    t.length = 57;
+    reset_passenger_array(&t);
 
     for (i = 0, flag0=1; i < 57; i++) {
-        if (passengers [i].destination !=0)
+        if (t.passengers [i].destination !=0)
             flag0=0;
-        if (passengers [i].location != -1)
+        if (t.passengers [i].location != -1)
             flag0=0;
-        if(passengers [i].carry_on != 0)
+        if(t.passengers [i].carry_on != 0)
             flag0=0;
-        if(passengers [i].wait_time != 0)
+        if(t.passengers [i].wait_time != 0)
             flag0=0;
-        if(passengers [i].finish != 0)
+        if(t.passengers [i].finish != 0)
             flag0=0;
-        if(passengers [i].interference_flag != 0)
+        if(t.passengers [i].interference_flag != 0)
             flag0=0;
-        if(passengers [i].spotting != NULL)
+        if(t.passengers [i].spotting != NULL)
             flag0=0;
     }
 
 
     CuAssertTrue(tc, flag0 == 1);
+    free(t.passengers);
 }
 
 /*tests that the destinations is between the legal seats
@@ -255,8 +255,11 @@ void test_reset_passenger_array(CuTest *tc){
 void test_get_random_array_array57(CuTest *tc){
     int i, j, flag0;
     int destination[60] = {0};
-
-    get_random_array(destination, 57,6, 4);
+    transition_system t;
+    t.seats_per_row = 6;
+    t.length = 57;
+    t.entrance = 4;
+    get_random_array(destination, &t);
 
     for(i=0, flag0=1; i<60; i++){
         if(!(destination[i]>=0 && destination[i]<60))
@@ -269,7 +272,6 @@ void test_get_random_array_array57(CuTest *tc){
                 flag0=0;
         }
     }
-
     CuAssertTrue(tc, flag0 == 1);
 }
 
@@ -278,8 +280,12 @@ void test_get_random_array_array57(CuTest *tc){
 void test_get_random_array_array15(CuTest *tc){
     int i, j, flag1;
     int destination1[18] = {0};
+    transition_system t;
+    t.seats_per_row = 6;
+    t.length = 15;
+    t.entrance = 2;
 
-    get_random_array(destination1, 15, 6, 4);
+    get_random_array(destination1, &t);
 
     for(i=0, flag1=1; i<18; i++){
         if(!(destination1[i]>=0 && destination1[i]<18))

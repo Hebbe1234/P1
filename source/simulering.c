b23d2561fdc1b968b411bfaa5b30449c75c65f1d
Gustav Sneholm Bruhns <gustavsneholmbruhns@h301.aau1x.klient2.satellit.site.aau.dk>
2019-11-29 09:22:54 +0100

    Starting to take shape

--- source/simulering.c
+++ source/simulering.c
@@ -19,12 +19,31 @@ int main (void) {
     
     transition_system t_system;
     
+    int destinationss[] = {0, 1, 2, 3};
+    int i;
+    passenger passengerss[4];
+    
     t_system.entrance = 0; 
     t_system.rows = 2;
-    
-    t_system.passengers[4];
+    t_system.destination_length = 4;
+    t_system.seats_per_row = 1;
+    t_system.destinations = &destinationss;
+    t_system.passengers = &passengerss;
     t_system.length = 2;
     
+    
+    passengerss[0].spotting = NULL;
+    passengerss[0].destination = 0;
+    passengerss[0].location = -1;
+    passengerss[0].finish = 0;
+
+    for (i = 1; i < t_system.length; ++i) {
+        passengerss[i].destination = i;
+        passengerss[i].location = -1;
+        passengerss[i].finish = 0;
+        passengerss[i].spotting = &(passengerss[i - 1]);
+    }
+
     simulation(t_system);
     return 0;
 }
@@ -32,7 +51,7 @@ int main (void) {
 void simulation(transition_system t_system) {
     int index;
 
-    while (is_finished(&t_system, t_system.length) == 0) {
+    while (is_finished(&t_system) == 0) {
         /* Finish */
         finalizing_passenger(&t_system);
         person_in_front(&t_system);

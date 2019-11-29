790acc8de36e11bef503dc8edaa031a31a43e18c
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2019-11-29 09:44:11 +0100

    add the update of person in front in initializing.c

--- source/initializing.c
+++ source/initializing.c
@@ -4,21 +4,28 @@
 #include "../headers/structs.h"
 
 #endif
+
+#include "../headers/rear.h"
+#include "../headers/print_passenger.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void initialize_passenger(transition_system *t) {
-    int i, 
-        P_L,
-        P_E = t->entrance;
-    
+    int i, P_L, P_E = t->entrance;
+
     passenger p;
 
     for (i = 0; i < t->length; i++) {
         p = t->passengers[i];
         P_L = p.location;
-        if (P_L == -1 && (p.spotting == NULL || ( p.spotting->location != P_E && p.spotting->location != -1 ))) {
+        if (P_L == -1 && (p.spotting == NULL || (p.spotting->location != P_E &&
+                                                 p.spotting->location != -1))) {
             t->passengers[i].location = P_E;
-        }    
-    }  
+            if(rear_function(t, i) != -1){
+                t->passengers[i].spotting = &(t->passengers[rear_function(t, i)]);
+            }
+            print_passenger_line(&(t->passengers[i]));
+        }
+    }
 }

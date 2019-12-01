99db667941fe0e9b887ba94de363bf29c784c56d
Hebbe1234 <r.hebsgaard@gmail.com>
2019-12-01 22:48:08 +0100

    Update inter.h

--- headers/inter.h
+++ headers/inter.h
@@ -5,5 +5,5 @@
 
 #endif
 
-/* Returns a given passengers waittime, based on the location of other passengers in their 'row' */
+/* Returns a given passengers waittime, based on the location of other passengers in their 'row' */ 
 int inter(transition_system *t, passenger p);

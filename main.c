e26772e0aa07e8d87b5f85499fd9baea001a3824
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2019-12-05 00:08:41 +0100

    implemented outside in boarding - needs tests and comments

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "headers/input.h"
 #include "headers/print_t_system.h"
 #include "headers/print_passenger.h"
+#include "headers/random_boarding_generator.h"
 
 #include <stdio.h>
 #include <math.h>
@@ -21,6 +22,26 @@ int silence = 0;
 int main (int argc, char *argv[])
 {
     int i;
+    transition_system t;
+    int *destination;
+
+    t.iterations = 1;
+    t.entrance = 9; 
+    t.rows = 10;                  /*bliver beregnet*/
+    t.destination_length = 4;    /*Bruges ikke indtilvidere*/
+    t.seats_per_row = 6;
+    t.length = 57;
+    t.carryon_percentage = 50;
+    t.wait.t_0 = 0;
+    t.wait.t_1 = 2;
+    t.wait.t_2 = 3;
+    t.wait.t_3 = 4;
+    t.wait.t_c = 5;
+    t.wait.t_m = 2;
+
+    destination = (int*)calloc(t.length+ t.seats_per_row/2, sizeof(int));
+
+
     srand(time(0));
     
     for (i = 0; i < argc; i++) {
@@ -28,7 +49,14 @@ int main (int argc, char *argv[])
             silence = 1;
         }
     }
+
+    outsidein_boarding_generator(&t, destination);
+
+
     printf("Program ran OK");
+    
+
+
 
     return EXIT_SUCCESS;
 }
--- source/random_boarding_generator.c
+++ source/random_boarding_generator.c
@@ -11,27 +11,44 @@
 /*Fills the given array with legal destinations that is shuffled so its similar to outside in boarding*/
 void outsidein_boarding_generator(transition_system *t, int *destinations) {
 
+    int i = 0, j = 0;
+    int **arrays = (int **)calloc(t->seats_per_row/2, sizeof(int)*2*t->rows);
 
-    
+    for(i = 0; i < t->seats_per_row/2; i++){
+        arrays[i] = (int *)calloc(2*t->rows, sizeof(int));
+    }
+
+    for(i = 0; i < t->seats_per_row/2; i++){
+       for(j = 0; j < t->rows; j++){
+            arrays[i][j] = 6*j+i;
+            arrays[i][t->rows+j] = 6*j + 5 - i;
+       }
+    }
+
+    for(i = 0; i < t->seats_per_row/2; i++){
+        shuffle_array(arrays[i], 2*t->rows);
+        for(j = 0; j < 2*t->rows; j++){
+            destinations[i*2*t->rows + j] = arrays[i][j];
+        }
+    } 
+
+    for(i = 0; i < t->rows*t->seats_per_row; i++){
+        printf("%d - %d\n",i, destinations[i]);
+    }
 }
 /*Fills the given array with legal destinations that is shuffled*/
 void random_boarding_generator(transition_system *t, int *random_destinations) {
-    int i, j, k, tmp_plads;
-    i = j = k = tmp_plads = 0;
+    int i = 0;
 
     for(i = 0; (i < t->length + (t->seats_per_row/2)); i++) {
+        random_destinations[i] = i;
+
         if( (is_legal_seat(i, t)) == 0) {
             random_destinations[i] = -1;
         }
-        random_destinations[i] = i;
     }
 
-    for(i = 0; i < 1000000; i++) {
-        get_random_number(&k,&j, t);
-        tmp_plads = random_destinations[k];
-        random_destinations[k] = random_destinations[j];
-        random_destinations[j] = tmp_plads;
-    }
+    shuffle_array(random_destinations, (t->length + (t->seats_per_row/2)));
 }
 
 /*Tests if the seat is replaced by a door, and therefore illegal*/
@@ -46,7 +63,19 @@ int is_legal_seat(int seat_tested, transition_system* t) {
 }
 
 /*Used to create random destination, by generating 2 random numbers*/
-void get_random_number(int *k,int *j,transition_system *t) {
-    *k = (rand() % (t->length + (t->seats_per_row/2)));
-    *j = (rand() % (t->length + (t->seats_per_row/2)));
+void get_random_number(int *k,int *j, int length) {
+    *k = (rand() % length);
+    *j = (rand() % length);
+}
+
+void shuffle_array(int *array, int length){
+    int i, k, j, tmp_plads;
+    i = k = j = tmp_plads = 0;
+
+     for(i = 0; i < 1000000; i++) {
+        get_random_number(&k,&j, length);
+        tmp_plads = array[k];
+        array[k] = array[j];
+        array[j] = tmp_plads;
+    }
 }

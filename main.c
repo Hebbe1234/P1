abb4bcdf93be5bbf86a3ca5fc63cb7f160c4dab2
sherlockina <you@example.com>
2019-12-09 12:54:25 +0100

    the resultso

--- main.c
+++ main.c
@@ -29,28 +29,16 @@ int main (int argc, char *argv[])
     int i, result = 0, start = 0;
     srand(time(NULL));
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
     for (i = 0; i < argc; i++) { 
         if (strcmp("--silence", argv[i]) == 0) {
             silence = 1;
         }
     }
-<<<<<<< HEAD
 
     start = time(0);
     result = run_simulations();
     printf("\nIT TOOK %d SECONDS\n", ((int)time(0)) - start);
 
-=======
-        
-    start = time(0);
-    result = run_simulations();
-    printf("\nIT TOOK %d SECONDS\n", time(0) - start);
-
->>>>>>> master
     printf("It took %d iterations on average\n", result);
     printf("Program ran OK");
     
